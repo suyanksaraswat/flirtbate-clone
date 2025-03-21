4be390b6762d99d537208122b6a7936313d873a8
Suyank Saraswat <suyank.saraswat@gmail.com>
2025-03-21 15:41:06 +0530

    fix: login modal styles

--- src/components/molecules/login/index.tsx
+++ src/components/molecules/login/index.tsx
@@ -3,6 +3,7 @@ import styles from "./login.module.css";
 import { FilledButton } from "@/components/atoms/button";
 import Input from "@/components/atoms/input";
 import { useState } from "react";
+import Link from "next/link";
 
 interface Props {
   open: boolean;
@@ -13,9 +14,15 @@ export default function Login({ open, onClose }: Props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleBackdropClick = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
   return open ? (
-    <div className={styles.loginBackdrop}>
-      <div className={styles.loginModalContainer}>
+    <div className={styles.loginBackdrop} onClick={handleBackdropClick}>
+      <div className={styles.loginModalContainer} onClick={(e) => e.stopPropagation()}>
         <div className={styles.leftSection}></div>
 
         <div className={styles.rightSection}>
@@ -40,7 +47,21 @@ export default function Login({ open, onClose }: Props) {
               placeholder={"Password"}
             />
 
+            <div className={styles.forgotPasswordContainer}>
+              <Link href={""}>Forgot Password?</Link>
+            </div>
+
             <FilledButton className={styles.loginButton}>Login</FilledButton>
+          </div>
+
+          <div className={styles.loginFooterContainer}>
+            <p>
+              Login as Modal <Link href={""}>here</Link>
+            </p>
+
+            <p>
+              Don't have an account? <b>Join for free now!</b>
+            </p>
           </div>
         </div>
       </div>
